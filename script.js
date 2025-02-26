0b34d4ef0fad435b1d7a2926999073a4c3b6fd94
Bhavya Modi <bhavyamodi873@gmail.com>
2025-02-26 15:32:48 -0500

    Fix responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function toggleMenu(){
+/**
+ * Toggles the mobile menu open/closed state
+ * Adds or removes the 'open' class to menu and hamburger icon
+ */
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
